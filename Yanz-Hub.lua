ebe1df366c7932d6ea3f897cdacc0db78e873e35
YANZ <165420181+lphisv5@users.noreply.github.com>
2025-09-30 02:47:42 +0700

    Update Yanz-Hub.lua

--- Yanz-Hub.lua
+++ Yanz-Hub.lua
@@ -4,7 +4,8 @@ local GameScripts = {
     [9285238704] = "https://github.com/lphisv5/rbxScript/raw/refs/heads/main/Race-Clicker.lua",
     [127742093697776] = "https://github.com/lphisv5/rbxScript/raw/refs/heads/main/PlantsVsBrainrots.lua",
     [6516141723] = "https://github.com/lphisv5/rbxScript/raw/refs/heads/main/Doors.lua",
-    [3956818381] = "https://github.com/lphisv5/rbxScript/raw/refs/heads/main/Ninja-Legends.lua"
+    [3956818381] = "https://github.com/lphisv5/rbxScript/raw/refs/heads/main/Ninja-Legends.lua",
+    [537413528] = "https://github.com/lphisv5/rbxScript/raw/refs/heads/main/BuildABoat.lua"
 }
 
 local url = GameScripts[game.PlaceId]

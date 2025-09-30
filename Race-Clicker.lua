aed983a512433121804a36814241d2657bd225cc
YANZ <165420181+lphisv5@users.noreply.github.com>
2025-09-30 22:38:11 +0700

    Update Race-Clicker.lua

--- Race-Clicker.lua
+++ Race-Clicker.lua
@@ -28,6 +28,7 @@ local HomeTab = Window:NewTab({
     Description = "Home Features",
     Icon = "rbxassetid://7733960981"
 })
+
 local HomeSection = HomeTab:NewSection({
     Title = "Home",
     Position = "Left"
@@ -55,6 +56,8 @@ HomeSection:NewButton({
         end
     end
 })
+
+
 local MainTab = Window:NewTab({Title="Main", Description="Auto System", Icon="rbxassetid://7733960981"})
 local AutoClickSection = MainTab:NewSection({Title="Auto Click", Icon="rbxassetid://7733916988", Position="Left"})
 local AutoWinsSection = MainTab:NewSection({Title="Auto Wins", Icon="rbxassetid://7733916988", Position="Right"})
@@ -64,12 +67,6 @@ local state = {
     autoClick = false,
     autoWins = false
 }
-
-
-
-
-
-
 
 
 --===[ Auto Click ]===--
